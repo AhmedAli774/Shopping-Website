57ea6ea7223c52177666135f162268c3ed2aa4d4
Ahmed Ali <121313364+AhmedAli774@users.noreply.github.com>
2023-03-12 19:56:59 +0500

    All File Upload
    File Upload

---
+++ java.js
@@ -0,0 +1,72 @@
+// const baar = document.getElementById("bar");
+// const nav = document.getElementById('menu');
+// const closes = document.getElementById('close');
+
+
+
+// if (baar) {
+// 	baar.addEventListener('click', () => {
+// 		nav.classList.add('active');
+// 	})
+// }
+
+// if (closes) {
+// 	closes.addEventListener('click', () => {
+// 		nav.classList.remove('active');
+// 	})
+// }
+// <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+
+ function myFunction() {
+            var c = document.getElementById("menu");
+            {
+                if (c.style.display === "block") {
+                    c.style.display ="";
+                }
+                else{
+                    c.style.display="block";
+                }
+            }
+        }
+
+
+        /*footer*/
+
+
+        	 function myFunctiona() {
+            var xx = document.getElementById("child");
+            {
+                if (xx.style.display === "block") {
+                    xx.style.display ="";
+                }
+                else{
+                    xx.style.display="block";
+                }
+            }
+            
+        }
+
+
+         function myFunctions() {
+            var y = document.getElementById("child2");
+            {
+                if (y.style.display === "block") {
+                    y.style.display ="";
+                }
+                else{
+                    y.style.display="block";
+                }
+            }
+        }
+          function myFunctionss() {
+            var z = document.getElementById("child3");
+            {
+                if (z.style.display === "block") {
+                    z.style.display ="";
+                }
+                else{
+                    z.style.display="block";
+                }
+            }
+        }
